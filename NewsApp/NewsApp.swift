9f1f775406da7e756d332371b7dbddf63c1fcacb
Feyyaz ONUR <feyyazonur0@gmail.com>
2024-01-10 17:23:07 +0300

    first commit

---
+++ NewsApp/NewsApp.swift
@@ -0,0 +1,18 @@
+//
+//  NewsApp.swift
+//  NewsApp
+//
+//  Created by Feyyaz ONUR on 10.01.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
