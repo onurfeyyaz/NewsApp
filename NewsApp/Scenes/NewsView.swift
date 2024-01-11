7ee375be889de51df35bcc12a8f57d511b6b0e26
Feyyaz ONUR <feyyazonur0@gmail.com>
2024-01-11 03:54:31 +0300

    add network to view and viewmodel

---
+++ NewsApp/Scenes/NewsView.swift
@@ -0,0 +1,64 @@
+//
+//  NewsViewView.swift
+//  NewsApp
+//
+//  Created by Feyyaz ONUR on 10.01.2024.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    @ObservedObject private var viewModel = NewsViewModel(repository: Repository(network: NetworkManager()))
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        NavigationSplitView {
+            VStack {
+                List {
+                    if let  articles = viewModel.articles {
+                        ForEach(articles, id:\.title) { article in
+                            NavigationLink(destination: DetailView()) {
+                                HStack {
+                                    VStack(alignment: .leading, spacing: 6) {
+                                        Text(article.title ?? "title")
+                                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                            .lineLimit(1)
+                                        Text(article.description ?? "description")
+                                            .font(.subheadline)
+                                            .lineLimit(3)
+                                    }
+                                    Spacer(minLength: 20)
+                                    Rectangle()
+                                        .frame(width: 80)
+                                        .redacted(reason: .placeholder)
+                                }
+                            }
+                        }
+                    }
+                }
+                .searchable(text: $searchText, prompt: "Haber Ara")
+                .onChange(of: searchText) { _, newValue in
+                    Task {
+                        viewModel.fetchNews(query: newValue, page: 1)
+                    }
+                }
+                
+            }
+            .navigationTitle("Appcent News")
+        } content: {
+            VStack {
+                Text("Content Screen")
+            }
+            .navigationTitle("content")
+        } detail: {
+            VStack {
+                Text("Detail Screen")
+            }
+            .navigationTitle("detail")
+        }
+    }
+}
+
+#Preview {
+    NewsView()
+}
--- NewsApp/ViewModels/HomeViewModel.swift
+++ NewsApp/ViewModels/HomeViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  HomeViewModel.swift
+//  NewsViewModel.swift
 //  NewsApp
 //
 //  Created by Feyyaz ONUR on 10.01.2024.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class HomeViewModel: ObservableObject {
+class NewsViewModel: ObservableObject {
     private let repository: Repository
     @Published var articles: [Article]?
     
@@ -15,11 +15,20 @@ class HomeViewModel: ObservableObject {
         self.repository = repository
     }
     
-    func fetchNews() async throws {
-        do {
-            self.articles = try await repository.retrieveNews()
-        } catch {
-            print("Error fetching news: \(error)")
+    func fetchNews(query: String, page: Int) {
+        repository.retrieveNews(query: query, page: page) { result in
+            switch result {
+            case .success(let articles):
+                DispatchQueue.main.async {
+                    self.articles = articles
+                }
+            case .failure(let error):
+                print("Error fetching news: \(error)")
+            }
         }
+    }
+    
+    func pagination() {
+        // TODO: pagination yap
     }
 }
